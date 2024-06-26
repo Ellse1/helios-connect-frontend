c25237814cef34e22b19e552b796d5fd6805d2aa
Elias Singer <elias.singer@online.de>
2024-06-26 18:25:29 +0200

    frontend green grid on landing page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,36 +79,50 @@ export default function Home() {
       </div>
 
 
-      <div className="w-2/3 mx-auto mt-20">
+      <div className="w-3/4 mx-auto mt-20">
         <div className="grid grid-cols-3 gap-4">
-          <div className='col-span-1'>
-            <div className='bg-green-200 p-4 rounded-xl'>
-                <h3 className='font-bold'>Überschrift 1</h3>
+          <div className='col-span-1 items-stretch'>
+            <div className='bg-green-200 p-4 rounded-xl h-full'>
+                <h3 className='font-bold'>Consumption Monitoring</h3>
                 <p>
-                  Lorem ipsum for some words Lorem ipsum for some words Lorem ipsum for some words Lorem ipsum for Lorem ipsum for  Lorem ipsum for Lorem ipsum for Lorem ipsum for Lorem ipsum for 
-                  Lorem ipsum for some words Lorem ipsum for Lorem ipsum for some words Lorem ipsum for 
+                  Always have an overview of the consumption, production and performance of your photovoltaik system.
                 </p>
+                <Image className='mx-auto mt-10' src="/images/Consumption-Monitoring.png" alt="Description of Image" width={100} height={100} />
             </div>
           </div>
           <div className='col-span-2 flex flex-col gap-4'>
             <div className='grid grid-cols-2 gap-4'>
-              <div className='bg-green-200 p-4 rounded-xl'>
-                <h3 className='font-bold'>Überschrift 2</h3>
-                Lorem ipsum for some words Lorem ipsum for Lorem ipsum for some words Lorem ipsum for 
+              <div className='col-span-2 bg-green-200 p-4 rounded-xl'>
+                <h3 className='font-bold'>Metering</h3>
+                <div className="grid grid-cols-6">
+                  <div className="col-span-4">
+                    Manual metering once a month to ensure correct invoices? Not with us and our fully automated metering in 15 minute intervals!
+                  </div>
+                  <div className="col-span-2">
+                    <Image src="/images/Metering.png" alt="Description of Image" width={100} height={100} />
+                  </div>
+                </div>
               </div>
               <div className='bg-green-200 p-4 rounded-xl'>
-                <h3 className='font-bold'>Überschrift 3</h3>
-                Some 2 Lorem ipsum for some words Lorem ipsum for Lorem ipsum for some words Lorem ipsum for 
-              </div>
+                <h3 className='font-bold'>Invoicing</h3>
+                We all know Germany is a beaurocratic mess. Keep your efforts low with our automated invoicing workflow.              </div>
+              <div className='bg-green-200 p-4 rounded-xl'>
+                <h3 className='font-bold'>Contract Management</h3>
+                Keeping contracts neatly organized in your cabinet? What’s next? Sending a Fax to your 20-year-old tenant?              </div>
             </div>
-            <div className='bg-green-200 p-4 rounded-xl h-full'>
-              <h3 className='font-bold'>Überschrift 4</h3>
-              Some 3
-              Lorem ipsum for some words Lorem ipsum for Lorem ipsum for some words Lorem ipsum for 
-            </div>
-
           </div>
         </div>
+        <div className="grid grid-cols-3 gap-4 w-full mt-4">
+          <div className="col-span-2 bg-green-200 p-4 rounded-xl">
+            <h3 className='font-bold'>Contributing to the energy transition</h3>
+            We think providing clean energy locally shouldn’t be a hassle. That’s why our vision is to keep your workload as small as possible while keeping your PV Systems’ yield as high as possible.
+          </div>
+          <div className="col-span-1 bg-green-200 p-4 rounded-xl">
+            <h3 className='font-bold'>Consultation</h3>
+            We bring your PV System from concept to up-and-running in no time with our professional consultation.
+          </div>
+        </div>
+
 
       </div>
 
